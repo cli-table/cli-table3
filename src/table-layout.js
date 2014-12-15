7772a3d7d7aa564eea02d0b6630924a82354994a
James Talmage <james.talmage@jrtechnical.com>
2014-12-15 02:19:03 -0500

    Code style cleanup

--- src/table-layout.js
+++ src/table-layout.js
@@ -1,99 +1,95 @@
 var _ = require('lodash');
 (function(){
+  var Cell = require('./cell');
 
-var Cell = require('./cell');
-
-function makeTableLayout(rows){
-  var cellRows = generateCells(rows);
-  expandCells(cellRows);
-  fillInTable(cellRows);
-  return cellRows;
-}
-
-function expandCells(cellRows){
-  for(var rowIndex = cellRows.length-1; rowIndex >= 0; rowIndex--){
-    var cellColumns = cellRows[rowIndex];
-    for(var columnIndex = 0; columnIndex < cellColumns.length; columnIndex++){
-      var cell = cellColumns[columnIndex];
-      for(var k = 1; k < cell.colSpan; k++){
-        cellColumns.splice(columnIndex+1,0,new Cell.NoOpCell());
-      }
-      for(var i = 1; i < cell.rowSpan; i ++){
-        var insertionRow = cellRows[rowIndex + i];
-        var spliceArgs = [columnIndex,0,new Cell.RowSpanCell(cell)];
-        for(var j = 1; j < cell.colSpan; j++){
-          spliceArgs.push(new Cell.NoOpCell());
-        }
-        insertionRow.splice.apply(insertionRow,spliceArgs);
-      }
-    }
+  function makeTableLayout(rows){
+    var cellRows = generateCells(rows);
+    expandCells(cellRows);
+    fillInTable(cellRows);
+    return cellRows;
   }
-}
 
-function fillInTable(rows){
-  var height = rows.length;
-  var width = maxWidth(rows);
-  for(var rowIndex = 0; rowIndex < height; rowIndex++){
-    var row = rows[rowIndex];
-    for(var colIndex = 0; colIndex < width; colIndex++){
-      var cell = row[colIndex];
-      if(!cell){
-        var i = colIndex+1;
-        while(i < width && !row[i]){
-          row[i] = new Cell.NoOpCell();
-          i++;
+  function expandCells(cellRows){
+    for(var rowIndex = cellRows.length-1; rowIndex >= 0; rowIndex--){
+      var cellColumns = cellRows[rowIndex];
+      for(var columnIndex = 0; columnIndex < cellColumns.length; columnIndex++){
+        var cell = cellColumns[columnIndex];
+        for(var k = 1; k < cell.colSpan; k++){
+          cellColumns.splice(columnIndex+1,0,new Cell.NoOpCell());
         }
-        var j = rowIndex + 1;
-        while(j < height && allBlank(rows[j],colIndex,i)){
-          for(var k = colIndex+1; k < i; k++){
-            rows[j][k] = new Cell.NoOpCell();
+        for(var i = 1; i < cell.rowSpan; i ++){
+          var insertionRow = cellRows[rowIndex + i];
+          var spliceArgs = [columnIndex,0,new Cell.RowSpanCell(cell)];
+          for(var j = 1; j < cell.colSpan; j++){
+            spliceArgs.push(new Cell.NoOpCell());
           }
-          j++;
-        }
-        var rowSpan = j - rowIndex;
-        var blankCell = new Cell(
-          {colSpan:i-colIndex,rowSpan: rowSpan}
-        );
-        row[colIndex] = blankCell;
-        for(var n = 1; n < rowSpan; n++){
-          rows[rowIndex+n][colIndex] = new Cell.RowSpanCell(blankCell);
+          insertionRow.splice.apply(insertionRow,spliceArgs);
         }
       }
     }
   }
-}
 
-function allBlank(row,from,to){
-  for(var i = from; i < to; i++){
-    if(row[i]) return false;
+  function fillInTable(rows){
+    var height = rows.length;
+    var width = maxWidth(rows);
+    for(var rowIndex = 0; rowIndex < height; rowIndex++){
+      var row = rows[rowIndex];
+      for(var colIndex = 0; colIndex < width; colIndex++){
+        var cell = row[colIndex];
+        if(!cell){
+          var i = colIndex+1;
+          while(i < width && !row[i]){
+            row[i] = new Cell.NoOpCell();
+            i++;
+          }
+          var j = rowIndex + 1;
+          while(j < height && allBlank(rows[j],colIndex,i)){
+            for(var k = colIndex+1; k < i; k++){
+              rows[j][k] = new Cell.NoOpCell();
+            }
+            j++;
+          }
+          var rowSpan = j - rowIndex;
+          var blankCell = new Cell(
+            {colSpan:i-colIndex,rowSpan: rowSpan}
+          );
+          row[colIndex] = blankCell;
+          for(var n = 1; n < rowSpan; n++){
+            rows[rowIndex+n][colIndex] = new Cell.RowSpanCell(blankCell);
+          }
+        }
+      }
+    }
   }
-  return true;
-}
 
-function generateCells(rows){
-  return _.map(rows,function(row){
-    return _.map(row,function(cell){
-      return new Cell(cell);
+  function allBlank(row,from,to){
+    for(var i = from; i < to; i++){
+      if(row[i]) return false;
+    }
+    return true;
+  }
+
+  function generateCells(rows){
+    return _.map(rows,function(row){
+      return _.map(row,function(cell){
+        return new Cell(cell);
+      });
     });
-  });
-}
-
-function maxWidth(rows){
-  return _.reduce(rows,function(maxWidth,row){
-    return Math.max(maxWidth,row.length);
-  },0);
-}
-
-function iterateColumn(rows,col,fn,ctx){
-  for(var i = 0; i < rows.length; i++){
-    fn.call(ctx,rows[i][col],i);
   }
-}
+
+  function iterateColumn(rows,col,fn,ctx){
+    for(var i = 0; i < rows.length; i++){
+      fn.call(ctx,rows[i][col],i);
+    }
+  }
+
+  function maxWidth(rows){
+    return _.reduce(rows,function(maxWidth,row){
+      return Math.max(maxWidth,row.length);
+    },0);
+  }
+
   function getCell(rows,y,x){
-    return rows[y][x];
-  }
-
-  function getCellPivot(rows,x,y){
     return rows[y][x];
   }
 
@@ -103,18 +99,22 @@ function iterateColumn(rows,col,fn,ctx){
       fn.call(ctx,columns[i],i);
     }
   }
+
   function maxHeight(rows){
     return rows.length;
   }
 
-module.exports = {
-  makeTableLayout:makeTableLayout,
-  maxWidth:maxWidth,
-  fillInTable:fillInTable,
-  computeWidths:makeComputeWidths(iterateColumn,maxWidth,'colSpan','desiredWidth',getCell),
-  computeHeights:makeComputeWidths(iterateRow,maxHeight,'rowSpan','desiredHeight',getCellPivot)
-};
+  function getCellPivot(rows,x,y){
+    return rows[y][x];
+  }
 
+  module.exports = {
+    makeTableLayout:makeTableLayout,
+    maxWidth:maxWidth,
+    fillInTable:fillInTable,
+    computeWidths:makeComputeWidths(iterateColumn,maxWidth,'colSpan','desiredWidth',getCell),
+    computeHeights:makeComputeWidths(iterateRow,maxHeight,'rowSpan','desiredHeight',getCellPivot)
+  };
 })();
 
 function makeComputeWidths(iterateColumn,maxWidth,colSpan,desiredWidth,getCell){
