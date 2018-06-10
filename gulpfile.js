91be35401ea356d65f23a93fdef658609fe0382f
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-06-10 18:42:50 +0200

    gulp: Extract `print-examples.js` script

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var printExample = require('./lib/print-example');
-
-gulp.task('example',function(){
-  printExample.logExample(require('./examples/basic-usage-examples'));
-  printExample.logExample(require('./examples/col-and-row-span-examples'));
-});
---
+++ scripts/print-examples.js
@@ -0,0 +1,4 @@
+var printExample = require('../lib/print-example');
+
+printExample.logExample(require('../examples/basic-usage-examples'));
+printExample.logExample(require('../examples/col-and-row-span-examples'));
