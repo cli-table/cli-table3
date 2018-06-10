6759accfdb5d43161a34a3d14b219a35a53202e7
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-06-10 17:47:37 +0200

    Table: Convert to ES6 class

--- src/table.js
+++ src/table.js
@@ -1,62 +1,69 @@
 var utils = require('./utils');
 var tableLayout = require('./layout-manager');
 
-function Table(options) {
-  this.options = utils.mergeOptions(options);
+class Table extends Array {
+  constructor(options) {
+    super();
+
+    this.options = utils.mergeOptions(options);
+  }
+
+  toString() {
+    var array = this;
+    var headersPresent = this.options.head && this.options.head.length;
+    if (headersPresent) {
+      array = [this.options.head];
+      if (this.length) {
+        array.push.apply(array, this);
+      }
+    } else {
+      this.options.style.head = [];
+    }
+
+    var cells = tableLayout.makeTableLayout(array);
+
+    cells.forEach(function(row) {
+      row.forEach(function(cell) {
+        cell.mergeTableOptions(this.options, cells);
+      }, this);
+    }, this);
+
+    tableLayout.computeWidths(this.options.colWidths, cells);
+    tableLayout.computeHeights(this.options.rowHeights, cells);
+
+    cells.forEach(function(row) {
+      row.forEach(function(cell) {
+        cell.init(this.options);
+      }, this);
+    }, this);
+
+    var result = [];
+
+    for (var rowIndex = 0; rowIndex < cells.length; rowIndex++) {
+      var row = cells[rowIndex];
+      var heightOfRow = this.options.rowHeights[rowIndex];
+
+      if (rowIndex === 0 || !this.options.style.compact || (rowIndex == 1 && headersPresent)) {
+        doDraw(row, 'top', result);
+      }
+
+      for (var lineNum = 0; lineNum < heightOfRow; lineNum++) {
+        doDraw(row, lineNum, result);
+      }
+
+      if (rowIndex + 1 == cells.length) {
+        doDraw(row, 'bottom', result);
+      }
+    }
+
+    return result.join('\n');
+  }
+
+  get width() {
+    var str = this.toString().split('\n');
+    return str[0].length;
+  }
 }
-
-Table.prototype.__proto__ = Array.prototype;
-
-Table.prototype.toString = function() {
-  var array = this;
-  var headersPresent = this.options.head && this.options.head.length;
-  if (headersPresent) {
-    array = [this.options.head];
-    if (this.length) {
-      array.push.apply(array, this);
-    }
-  } else {
-    this.options.style.head = [];
-  }
-
-  var cells = tableLayout.makeTableLayout(array);
-
-  cells.forEach(function(row) {
-    row.forEach(function(cell) {
-      cell.mergeTableOptions(this.options, cells);
-    }, this);
-  }, this);
-
-  tableLayout.computeWidths(this.options.colWidths, cells);
-  tableLayout.computeHeights(this.options.rowHeights, cells);
-
-  cells.forEach(function(row) {
-    row.forEach(function(cell) {
-      cell.init(this.options);
-    }, this);
-  }, this);
-
-  var result = [];
-
-  for (var rowIndex = 0; rowIndex < cells.length; rowIndex++) {
-    var row = cells[rowIndex];
-    var heightOfRow = this.options.rowHeights[rowIndex];
-
-    if (rowIndex === 0 || !this.options.style.compact || (rowIndex == 1 && headersPresent)) {
-      doDraw(row, 'top', result);
-    }
-
-    for (var lineNum = 0; lineNum < heightOfRow; lineNum++) {
-      doDraw(row, lineNum, result);
-    }
-
-    if (rowIndex + 1 == cells.length) {
-      doDraw(row, 'bottom', result);
-    }
-  }
-
-  return result.join('\n');
-};
 
 function doDraw(row, lineNum, result) {
   var line = [];
@@ -66,10 +73,5 @@ function doDraw(row, lineNum, result) {
   var str = line.join('');
   if (str.length) result.push(str);
 }
-
-Table.prototype.__defineGetter__('width', function() {
-  var str = this.toString().split('\n');
-  return str[0].length;
-});
 
 module.exports = Table;
