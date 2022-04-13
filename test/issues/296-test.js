16e73d147d68db8299b07dda1431e8f4fe36fdfa
speedytwenty <speedytwenty@gmail.com>
2022-04-12 20:01:57 -0600

    Failing (non-completing) tests for infinite loop in Cell.drawEmpty()

---
+++ test/issues/296-test.js
@@ -0,0 +1,31 @@
+const Table = require('../..');
+
+/**
+ * This test doesn't actually fail it never completes. I could not find a method
+ * to get the infinite loop to timeout within the test. Not sure how GHA will
+ * handle this.
+ */
+test('it should not loop infinitely with invalid table data', async () => {
+  const table = new Table();
+  table.push(
+    [
+      { content: 'A', colSpan: 2 },
+      { content: 'B', rowSpan: 3 },
+    ],
+    [],
+    [{ content: 'C', colSpan: 3 }]
+  );
+  expect(() => table.toString()).not.toThrow();
+});
+
+test('it should not error on invalid table data', () => {
+  const table = new Table();
+  table.push(
+    [
+      { content: 'A', colSpan: 2 },
+      { content: 'B', rowSpan: 3 },
+    ],
+    [{ content: 'C', colSpan: 3 }]
+  );
+  expect(() => table.toString()).not.toThrow();
+});
